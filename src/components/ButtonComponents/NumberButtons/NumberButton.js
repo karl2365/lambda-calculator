91a9bbc4d0c61523f94a5cf03c6a1cefbedf5bdc
Karl Beksa <karlbeksa@gmail.com>
2019-07-09 16:51:39 -0400

    calculator styled zero fixed

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -3,10 +3,11 @@ import '../buttons.css';
 
 const NumberButton = (props) => {
   console.log(props);
+
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button className='numberButton'>{props.number}</button>
+      {props.number === '0' ? <button className='zero'>{props.number}</button> :  <button className='numberButton'>{props.number}</button>}
     </>
   );
 };
